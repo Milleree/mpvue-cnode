1c80495ed1a107d95926137cdfbd5c30dfb8a758
bowen zhao <z.bowen66@gmail.com>
2018-04-02 13:07:10 +0800

    删除列表页的content字段

--- src/services/topics.js
+++ src/services/topics.js
@@ -4,6 +4,13 @@ export const getTopics = ({ page = 1, limit = 20 }) => $wx.request({
   url: `/topics?page=${page}&limit=${limit}`,
   methods: 'GET',
 })
+  .then(topics => {
+    // content字段数据量太大，列表页也没有用到，所以删掉
+    topics.forEach(topic => {
+      delete topic.content
+    })
+    return topics
+  })
 
 export const getDetail = ({ id }) => $wx.request({
   url: `/topic/${id}`,
